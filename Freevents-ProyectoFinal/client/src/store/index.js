c209f53f3ba423910963edf0c8d7b25107c20635
Hosmar Carrillo <hosmarcarrillo@gmil.com>
2022-09-14 15:53:11 -0300

    correccion de error en store

--- Freevents-ProyectoFinal/client/src/store/index.js
+++ Freevents-ProyectoFinal/client/src/store/index.js
@@ -1,6 +1,6 @@
 import {createStore, applyMiddleware} from "redux";
-import {composeWithDevtools} from "redux-devtools-extension";
+import {composeWithDevTools} from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "../reducer";
 
-export const store = createStore(rootReducer, composeWithDevtools(applyMiddleware(thunk)))
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
