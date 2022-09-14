26934baee836a85a001034b12679334e44cc08e0
Jessica Biagioni <100663378+JessicaBiagioni@users.noreply.github.com>
2022-09-14 14:42:14 -0300

    segundo commit

--- Freevents-ProyectoFinal/client/src/App.js
+++ Freevents-ProyectoFinal/client/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 
 function App() {
   return (
+    <BrowserRouter>
+    <Routes>
     <div className="App">
       <h1>AGUANTE FREEVENTS</h1>
     </div>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ Freevents-ProyectoFinal/client/src/store/index.js
@@ -0,0 +1,6 @@
+import {createStore, applyMiddleware} from "redux";
+import {composeWithDevtools} from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "../reducer";
+
+export const store = createStore(rootReducer, composeWithDevtools(applyMiddleware(thunk)))
