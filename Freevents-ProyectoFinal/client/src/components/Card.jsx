0e334c59bed1fe0c8754e7e5d14024d14d10cb72
Jessica Biagioni <100663378+JessicaBiagioni@users.noreply.github.com>
2022-09-15 14:12:40 -0300

    jess

---
+++ Freevents-ProyectoFinal/client/src/components/Card.jsx
@@ -0,0 +1,21 @@
+// import React from "react";
+// import {Link} from "react-router-dom";
+// import image13 from "../imagenes/13.jpeg";
+
+// const Card = ({name, id, description}) => {
+// return(
+//     <div>
+//         <Link to={`/detail/${id}`}>
+//         <img src={image13} alt=""/>
+//         <h3> {name} </h3>
+//         <div>
+//         {description?.map((description, index) => (
+//                             <button key={index}>{description}</button>
+//                         ))}
+//         </div>
+//         </Link>
+//     </div>
+// )
+// }
+
+// export default Card;
