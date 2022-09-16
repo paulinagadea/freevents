8dcc08627518d1802967ac2f64bc204ea6ba9b10
Jessica Biagioni <100663378+JessicaBiagioni@users.noreply.github.com>
2022-09-16 02:06:25 -0300

    modificasimple

--- Freevents-ProyectoFinal/client/src/components/Card.jsx
+++ Freevents-ProyectoFinal/client/src/components/Card.jsx
@@ -2,17 +2,22 @@
 // import {Link} from "react-router-dom";
 // import image13 from "../imagenes/13.jpeg";
 
-// const Card = ({name, id, description}) => {
+// const Card = ({name, id, description, price, type_service}) => {
 // return(
 //     <div>
 //         <Link to={`/detail/${id}`}>
 //         <img src={image13} alt=""/>
 //         <h3> {name} </h3>
 //         <div>
-//         {description?.map((description, index) => (
-//                             <button key={index}>{description}</button>
+//         {type_service?.map((typeservice, index) => (
+//                             <button key={index}>{typeservice}</button>
 //                         ))}
 //         </div>
+//<div>
+//    {events?.map((event, index) => (
+//        <button key={index}>{event}</button>
+//    ))}
+//</div>
 //         </Link>
 //     </div>
 // )
