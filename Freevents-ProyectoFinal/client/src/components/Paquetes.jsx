1e63d96f4930baf5e266d82e344163aa0d332729
Jessica Biagioni <100663378+JessicaBiagioni@users.noreply.github.com>
2022-09-16 03:50:49 -0300

    nuevoscomponentes

---
+++ Freevents-ProyectoFinal/client/src/components/Paquetes.jsx
@@ -0,0 +1,16 @@
+<div>
+<select>
+    <option selected disabled>
+        Orden Alfabetico
+    </option>
+    <option value="A-Z">A-Z</option>
+    <option value="Z-A">Z-A</option>
+</select>
+<select>
+    <option selected disabled>
+        Price
+    </option>
+    <option value="0-5">0-5</option>
+    <option value="5-0">5-0</option>
+</select>
+</div>
