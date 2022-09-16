0b653fe59acb5a72b72e056a4ec65790844b3a13
MarcoAlayn <marc_rob92@hotmail.com>
2022-09-16 05:57:34 -0700

    rutaGetAllProviders Fixed

--- Freevents-ProyectoFinal/api/src/routes/Provider.js
+++ Freevents-ProyectoFinal/api/src/routes/Provider.js
@@ -1,22 +1,48 @@
 const { Router } = require('express');
 const { getAllProviders } = require('../controllers/getAllProviders')
-// const { getProviderByName } = require('../controllers/getProviderByName')
-
 const router = Router();
+// const { Provider } = require('../db')
+
 
 router.get('/', async (req, res) => {
     try {
-        // const { name } = req.query
-        const allProviders = await getAllProviders()
-        // const providersByName = await getProviderByName(name)
 
-        name &&
-            // res.status(200).json(providersByName)
-            res.status(200).json(allProviders)
+        // const provedores = await Provider.findAll()
+        const provedores = await getAllProviders()
+
+        res.status(200).send(provedores)
+
 
     } catch (error) {
         res.status(500).json({ message: 'Error', error })
     }
 })
+
+// router.post("/", async (req, res) => {
+//     const { name, adress, location, postal_code, cuit, email, phone_number } = req.body;
+//     try {
+//         const actCreated = await Activity.create({
+//             name,
+//             adress,
+//             location,
+//             postal_code,
+//             cuit,
+//             email,
+//             phone_number
+//         })
+//         // for (let i = 0; i < countries.length; i++) {
+//         //     const countryActivity = await Country.findOne({
+//         //         where: { name: countries[i] }
+//         //     })
+//         //     actCreated.addCountry(countryActivity);
+//         // }
+
+
+//         res.status(200).json(actCreated);
+//     } catch (error) {
+//         res.status(500).json({ msg: 'Error', error })
+//     }
+// });
+
 
 module.exports = router;
