62ce3199ae69be9f196decd83ab15600ae48d4e6
Paulina <paulinagadea2702@gmail.com>
2022-09-17 20:04:57 -0300

    ruta getServiceByName lista

--- Freevents-ProyectoFinal/api/src/routes/Service.js
+++ Freevents-ProyectoFinal/api/src/routes/Service.js
@@ -1,14 +1,25 @@
 const { Router } = require('express');
-const { getAllServices } = require('../controllers/getAllServices.js')
+const { getAllServices, getServiceByName } = require('../controllers/getAllServices.js')
 
 const router = Router();
 
 router.get('/', async (req, res) => {
+    const { name } = req.query;
     try {
-        const allServices = await getAllServices()
-        res.status(200).json(allServices);
+        // Si no recibo un nombre por query muestro todos los servicios.
+        if (!name) {
+            const services = await getAllServices();
+            res.status(200).send(services);
+        } else {
+            const service = await getServiceByName(name);
+            if (service.length) {
+                res.status(200).send(service);
+            } else {
+                res.status(404).send("Service not found");
+            };
+        };
     } catch (error) {
-        res.status(404).send("Services not found");
+        res.send(error);
     };
 });
 
