eb966a97657a6db355cc5c539fd0b4493ec678b4
Paulina <paulinagadea2702@gmail.com>
2022-09-17 14:56:48 -0300

    error de tipeo en la ruta solucionado

--- Freevents-ProyectoFinal/api/src/controllers/getAllServices.js
+++ Freevents-ProyectoFinal/api/src/controllers/getAllServices.js
@@ -1,14 +1,8 @@
 const { Service } = require('../db')
 
 const getAllServices = async () => {
-    try {
-        const service = await Service.findAll({
-
-        });
+        const service = await Service.findAll();
         return service;
-    } catch (error) {
-        res.send(error);
-    };
 };
 
 module.exports = { getAllServices }
