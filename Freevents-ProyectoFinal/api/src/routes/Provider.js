148d1ea7d0a46f2afae4b19190279664e0646443
MarcoAlayn <marc_rob92@hotmail.com>
2022-09-18 19:20:01 -0700

    ruta_postProvider_fix

--- Freevents-ProyectoFinal/api/src/routes/Provider.js
+++ Freevents-ProyectoFinal/api/src/routes/Provider.js
@@ -1,41 +1,17 @@
 const { Router } = require('express');
 const { getAllProviders } = require('../controllers/getAllProviders')
-const { getAllProviderByName } = require('../controllers/getProviderByName')
-const { getProviderById } = require('../controllers/getProviderById')
 const router = Router();
-const { Provider } = require('../db')
+const { Provider, Event } = require('../db')
 
 router.get('/', async (req, res) => {
     try {
-        const { name } = req.query
         const provedores = await getAllProviders()
-        const provedorByName = await getAllProviderByName(name)
-
-        console.log('esto es provedorByName', provedorByName)
-        name ?
-            res.status(200).json(provedorByName)
-            : res.status(200).json(provedores)
-
+        res.status(200).json(provedores)
 
     } catch (error) {
         res.status(500).json({ message: 'Error', error })
     }
 })
-
-router.get('/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-        const provedorById = await getProviderById(id)
-
-        console.log('esto es provedorById', provedorById)
-
-        res.status(200).json(provedorById)
-
-    } catch (error) {
-        res.status(500).json({ message: 'Error', error })
-    }
-})
-
 
 router.post("/", async (req, res) => {
     const { name, adress, location, postal_code, cuit, email, phone_number } = req.body;
@@ -49,9 +25,12 @@ router.post("/", async (req, res) => {
             email,
             phone_number,
             logotype,
-            background_id,
+            background_image,
             galery_image
         })
+
+
+        //
 
         res.status(200).json(actCreated);
     } catch (error) {
