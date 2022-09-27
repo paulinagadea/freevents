197fed1db3ee9fababb36250cfb7fe725f5c8053
karencabrera27 <kareeencabrera3@gmail.com>
2022-09-27 18:59:08 -0300

    dato user

--- Freevents-ProyectoFinal/client/src/components/PerfilUser.jsx
+++ Freevents-ProyectoFinal/client/src/components/PerfilUser.jsx
@@ -1,38 +1,50 @@
 import React from "react";
-//import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { Chip } from "@material-ui/core";
 import { Avatar } from "@mui/material";
-import { useDispatch } from "react-redux";
-import { auth } from "../firebase";
+import './PerfilUser.css'
+import { useAuth } from "../context/AuthContext";
+import { auth  } from "../firebase";
 
 
 const PerfilUser = () => {
 
-    // const { user } = auth;
-    // let misFavs = JSON.parse(localStorage.getItem("favs"));
-    // let favsFromLocalStorage = misFavs ? misFavs : "No tienes Favoritos"
+    const { favs } = useSelector((state) => state.favs)
+
+    // let y = JSON.parse(localStorage.getItem(favs));
+    // let favsFromLocalStorage = y ? y : "No hay Favoritos"
+    const data = auth.currentUser.providerData[0];
+
+    return (
+        <div>
+            <Chip avatar={<Avatar src="https://cloudfront-us-east-1.images.arcpublishing.com/radiomitre/7CGFXAZMJJGNFGQAFMABMXYBMU.png" alt="" />} />
+            <div><h1 className="portada">Portada</h1></div>
+
+            <div className="titulo">
+                <h1>Bienvenido {data.displayName}</h1>
+            </div>
+
+            
+
+            <div>
+                {favs?.map((f) => {
+                    return (
+                        <div>
+                            {f.name}
+                        </div>
+                    )
+                })}
+
+            </div>
+
+            <Link to="/paquetes">
+                <button>Volver</button>
+            </Link>
 
 
-
-
-    // if (user) return (
-    //     <div>
-    //         <Chip avatar={<Avatar src="https://cloudfront-us-east-1.images.arcpublishing.com/radiomitre/7CGFXAZMJJGNFGQAFMABMXYBMU.png" alt="" />} />
-    //         <div>
-    //             <h1>Favoritos</h1>
-    //         </div>
-    //         <div>
-    //             {favsFromLocalStorage.length === 0 ? " No hay Favoritos" : favsFromLocalStorage}
-    //         </div>
-
-    //         <Link to="/paquetes">
-    //             <button>Volver</button>
-    //         </Link>
-
-
-    //     </div>
-//     )
+        </div>
+    )
 }
 
 export default PerfilUser;
