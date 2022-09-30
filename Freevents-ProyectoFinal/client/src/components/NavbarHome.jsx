35a9a8869e19de8f7d47808e86acef35082ea4fe
Paulina <paulinagadea2702@gmail.com>
2022-09-30 02:20:56 -0300

    cambio en el home x7467838929

--- Freevents-ProyectoFinal/client/src/components/NavbarHome.jsx
+++ Freevents-ProyectoFinal/client/src/components/NavbarHome.jsx
@@ -6,7 +6,7 @@ import MenuItem from '@mui/material/MenuItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
+// import Typography from '@mui/material/Typography';
 import Tooltip from '@mui/material/Tooltip';
 import PersonAdd from '@mui/icons-material/PersonAdd';
 import Settings from '@mui/icons-material/Settings';
@@ -21,8 +21,6 @@ import Hidden from '@mui/material/Hidden';
 // import { useAuth } from "../context/AuthContext";
 // import Salir from "./Salir"
 import { useAuth0 } from "@auth0/auth0-react";
-
-
 
 export default function NavbarHome() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -57,7 +55,7 @@ export default function NavbarHome() {
       <div>
 
         <NavContainer> 
-        <Link className="link" to="/home">Freevents</Link>
+        <Link className='link' to="/home">Freevents</Link>
           {/* <SearchBar/> */}
         <div className="containerbar">
           {
@@ -75,6 +73,7 @@ export default function NavbarHome() {
           
           {/* {(estadoUser === "provider"  ||   estadoUser === "user") && <Button size="small" onClick={handleClick} href="/proveedores">Proveedores</Button>} */}
           <Button size="small" onClick={handleClick} href="/paquetes">Paquetes</Button>
+          <Button size="small" onClick={handleClick} href="/devs">Nosotros</Button>
           
           {/* <Salir/> */}
           {/* <Button size="small" onClick={handleClick} href="/eventos">Crea tu evento</Button> */}
@@ -86,11 +85,11 @@ export default function NavbarHome() {
   }
   
   const NavContainer= styled.nav`
+  
   .link{
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
     font-size: 50px;
     margin-left: 5%;
-    font-family: 'Epilogue', sans-serif;
+    font-family: 'Mollie Glaston';
   }
 
  .containerbar{
