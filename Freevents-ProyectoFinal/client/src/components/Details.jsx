7b71c72067f191baf8c7d371c21b5f68a23b0d12
Hosmar Carrillo <hosmarcarrillo@gmil.com>
2022-10-01 12:56:09 -0300

    deteil packs

--- Freevents-ProyectoFinal/client/src/components/Details.jsx
+++ Freevents-ProyectoFinal/client/src/components/Details.jsx
@@ -7,17 +7,30 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getDetails, clearDetails, getEvents, getDetailsPacks, getPacks } from "../actions"
 import { useEffect } from 'react'
 import { Container } from '@material-ui/core';
+import CardPaquetes from './CardPaquetes'
+import { Button } from '@mui/material';
 
 const Details = () => {
     const dispatch = useDispatch()
     let { id } = useParams()
     const myservice = useSelector((state) => state.detail)
+    console.log("🚀 ~ file: Details.jsx ~ line 15 ~ Details ~ myservice", myservice)
     const evente = useSelector((state) => state.events)
-    console.log("🚀 ~ file: Details.jsx ~ line 16 ~ Details ~ evente", evente)
     const detallePack = useSelector((state) => state.detailPack)
-    console.log("🚀 ~ file: Details.jsx ~ line 17 ~ Details ~ detallePack", detallePack)
     const packs = useSelector((state) => state.packs)
+    const nombrepack=packs.name
+    console.log("🚀 ~ file: Details.jsx ~ line 20 ~ Details ~ nombrepack", nombrepack)
     console.log("🚀 ~ file: Details.jsx ~ line 19 ~ Details ~ packs", packs)
+    const auxProvId = myservice.id
+    console.log("🚀 ~ file: Details.jsx ~ line 21 ~ Details ~ auxProvId", auxProvId)
+    const auxPackId = packs.map((idPack) => idPack.providerId)
+    console.log("🚀 ~ file: Details.jsx ~ line 25 ~ Details ~ auxPackId", auxPackId)
+    const verificacion = auxPackId.includes(auxProvId)
+    const coincidencia = packs.filter((e)=>e.providerId===auxProvId)
+    console.log("🚀 ~ file: Details.jsx ~ line 27 ~ Details ~ coincidencia", coincidencia)
+    
+
+
     
 
 
@@ -91,6 +104,35 @@ const Details = () => {
                                     {/* </div> */}
                                 </div>
                             </div >
+                            <div>
+                    {
+                        verificacion?
+                        coincidencia.map((packs)=>{
+                            return(
+                                <div className='item'>
+                                            <Link style={{ textDecoration: "none" }} to={`/detailPaquete/${packs.id}`}>
+                                                <CardPaquetes
+                                                name={packs.name}
+                                                price={packs.price}
+                                                galery_image={packs.galery_image ? packs.galery_image : "https://www.dondeir.com/wp-content/uploads/2018/09/fiesta-1.jpg"}
+                                                />
+                                            </Link>
+                                            </div>
+
+                                // <Button href={`/detailPaquete/${e.id}`}>
+                                // <div>
+                                // <p>{e.name}</p>
+                                // <p>{e.description}</p>
+                                // <p>{e.price}</p>
+                                // </div>
+                                // {/* </Link> */}
+                                // </Button>
+                            )
+                        }) 
+                    : 
+                    <p>No hay paquetes</p>
+                    }
+                </div>
                         </div>
                     </Container>
                     <div className="conteinerd">
@@ -108,15 +150,17 @@ const Details = () => {
                         </div>
                     </div>
 
-                    <div>
+                    {/* <div>
                         {
                             detallePack && detallePack.id ? (
                                 <div><h1>detalles</h1></div>
                             ) : <h1>No hay nada </h1>            
                         }
-                    </div>
+                    </div> */}
+
                 </div>
             )
+
             : <div>
                 <p>Loading...</p>
                 <img
