6849f7050f52b93963f1c3ab642a76390eb1b3f8
Jessica Biagioni <100663378+JessicaBiagioni@users.noreply.github.com>
2022-10-03 14:24:19 -0300

    favoritos

--- Freevents-ProyectoFinal/client/src/components/PerfilUser.jsx
+++ Freevents-ProyectoFinal/client/src/components/PerfilUser.jsx
@@ -1,41 +1,39 @@
 import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import './Profile.css';
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch,  } from "react-redux";//useSelector
 import { Link } from "react-router-dom";
-import {getOrder} from '../actions'
-import { getPacks, getServices} from "../actions";
+import { getOrder } from '../actions'
+//import { getPacks, getServices} from "../actions";
 import CardOrden from './CardOrden'
-// import Navigate from 'react-router-dom'
+
 
 export default function PerfilUser() {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
   // const { favs } = useSelector((state) => state.favs)
-  const ordenes = useSelector((state) => state.ordercita)
-  console.log(ordenes, 'orden')
-
+  //const ordenes = useSelector((state) => state.ordercita)
+ // const providersO = useSelector((state)=>state.providers)
   
 
 
   useEffect(() => {
     dispatch(getOrder())
   }, [dispatch])
-  
+
   const { user, isAuthenticated, isLoading } = useAuth0();
-  // const {email, name, }
-    // console.log(first)
-    console.log(useAuth0())
-    // const navigate = Navigate();
+
+  console.log(useAuth0())
   if (isLoading) {
     return <div>Loading...</div>;
-
   }
-  // let y = JSON.parse(localStorage.getItem(favs));
-  // let favsFromLocalStorage = y ? y : "No hay Favoritos"
-  // const data = auth.currentUser.providerData[0];
+
+
+  const y = JSON.parse(localStorage.getItem("favs"));
+  //const data = auth.currentUser.providerData[0];
+  const order = JSON.parse(localStorage.getItem("order"));
 
   return (
-    // isAuthenticated && (
+    // isAuthenticated?  (
     //   <div>
 
     //     <div><h1 className="portada"></h1></div>
@@ -45,68 +43,66 @@ export default function PerfilUser() {
     //     <p>Email: {user.email}</p>
     //   </div>
     // )
-    
-  <div>
-    {/* <Chip avatar={<Avatar src={data.photoURL} alt="" />} /> */}
+
     <div>
       <div>
-          <div><h1 className="portada">Portada</h1></div>
-
-          <img className="foto-perfil" src={user.picture} alt={user.name} />
+        <div>
+          <div>
+            <h1 className="portada">Mi Perfil</h1></div>
+          <img className="foto-perfil" src={user.picture ? user.picture : false} alt={user.name} />
           <h1 className="titulo">Bienvenido {user.name}</h1>
-      </div>
-      {/* hacer logica comparando el uuid del auth0 con el id de cliente */}
-      <div className="cont-info">
-        <div className="info">
-          <h3>Mis compras</h3>
-          <div className="compras">
-            {
-              ordenes?.map((ord) =>{
-                return(
-                  <div>
-                    <CardOrden
-                      pack_service={ord.pack_service ? ord.pack_service.name : <p>Campo incompleto</p>}
-                      price = {ord.price}
-                      status={ord.status}
-                      provider = {ord.provider ? ord.provider.name : <p>No tiene proveedor</p>}
-                      event_address={ord.event_address}
-                    />
-                  </div>
-                )
-              })
-            }
+          <div className="info">
+            <h3>Mis datos</h3>
+            <div className="datos">
+              <p>Nombre: {user.given_name}</p>
+              <p>Apellido: {user.family_name}</p>
+              <p>Email: {user.email}</p>
+            </div>
           </div>
         </div>
-
-        <div className="info">
-          <h3>Mis datos</h3>
-          <div className="datos">
-            <p>Nombre: {user.given_name}</p>
-            <p>Apellido: {user.family_name}</p>
-            <p>Email: {user.email}</p>
+        {/* hacer logica comparando el uuid del auth0 con el id de cliente */}
+        <div className="cont-info">
+          <div className="info">
+            <h3>MIS COMPRAS</h3>
+            <div className="compras">
+              {
+                order?.map((ord) => {
+                  return (
+                    <div>
+                      <CardOrden
+                        pack_service={ord.pack_service ? ord.pack_service.name : false}
+                        price={ord.price}
+                        status={ord.status}
+                        provider={ord.provider ? ord.provider.name : false}
+                        event_address={ord.event_address? ord.event_address :false}
+                      />
+                    </div>
+                  )
+                })
+              }
             </div>
-        </div>
-      </div>
-
-    {/* <div>
-        {favs?.map((f) => {
+          </div> 
+          <div className="info">
+            <div className="compras">
+          <h3>FAVORITOS</h3>
+          {y ? y.map((f) => {
             return (
-                <div>
-                    {f.name}
-                </div>
+              <div>
+                <ul>
+                🤍 {f.name}
+                </ul>
+              </div>
             )
-        })}
+          }) : "No hay Favoritos"}
+          </div>
+        </div>
+        </div>
 
-    </div> */}
-
-    <Link to="/home">
-        <button>Volver</button>
-    </Link>
-
+        <Link to="/home">
+          <button>Volver</button>
+        </Link>
+      </div>
     </div>
-  </div>
   )
-
-      
 };
 
